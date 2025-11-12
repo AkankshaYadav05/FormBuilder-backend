82b8f768768bfca1991dee84866ab2b05faa1c2b
Akanksha Yadav <rd.akanksha05@gmail.com>
2025-11-12 20:19:41 +0530

    changed

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ const app = express();
 // ===== CORS =====
 app.use(cors({
   origin: [
-    "https://form-builder-production-cee9.up.railway.app",
     "http://localhost:5173"
    ] ,
   credentials: true,
@@ -91,39 +90,14 @@ app.get("/api/test", (req, res) => {
 });
 
 
-isConnected = false;
-async function connectToMongoDB() {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-    isConnected = true;
-    console.log('Connected to MongoDB');
-  } catch(error) {
-    console.error('Error connecting to MongoDB:', error);
-  }
-
-  // add middleware
-
-  app.use((req,res,next) => {
-    if(!isConnected) {
-      connectToMongoDB()
-    }
-    next();
-  })
-}
-
-
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // ===== MongoDB Connection & Start Server =====
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("MongoDB connected");
-//     app.listen(PORT, () => console.log("Server running on port 5000"));
-//   })
-//   .catch(err => console.error("MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => console.log("Server running on port 5000"));
+  })
+  .catch(err => console.error("MongoDB connection error:", err));
 
 
-export default app;
